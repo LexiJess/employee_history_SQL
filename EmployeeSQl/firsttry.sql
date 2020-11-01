0975d4f87b235d1de4a79c48273c03002d3c0e9a
LexiJess <32692730+LexiJess@users.noreply.github.com>
2020-10-31 21:53:37 -0600

    uploading files

--- EmployeeSQl/firsttry.sql
+++ EmployeeSQl/firsttry.sql
@@ -1,1 +1,9 @@
 This is my first attempt at this file. 
+
+CREATE TABLE bird_song (
+	english_name VARCHAR,
+	country VARCHAR,
+	latitude DEC,
+	longitude DEC
+);
+
---
+++ EmployeeSQl/uploaded _csv_files.sql
@@ -0,0 +1,1 @@
+uploaded the csv files. 

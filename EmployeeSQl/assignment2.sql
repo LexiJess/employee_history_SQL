21be30ef17572a5f6446d9fa2cef1f11c174d46d
LexiJess <32692730+LexiJess@users.noreply.github.com>
2020-11-09 21:56:53 -0700

    quit dropping columns

--- EmployeeSQl/assignment2.sql
+++ EmployeeSQl/assignment2.sql
@@ -10,4 +10,9 @@ ALTER TABLE "assignment_2" DROP COLUMN "birth_date";
 ALTER TABLE "assignment_2" DROP COLUMN "sex";
 ALTER TABLE "assignment_2" DROP COLUMN "emp_no";
 
-SELECT * FROM "assignment_2" WHERE "hire_date" LIKE date('1986%');
+--SELECT * FROM "assignment_2" WHERE "hire_date" > ('1985-12-31') and "hire_date"< ('1987-01-01');
+
+SELECT * FROM "assignment_2" WHERE "hire_date" BETWEEN ('1986-01-01') and ('1986-12-31');
+
+SELECT "first_name", "last_name", "hire_date" FROM "Data_Employees" WHERE "hire_date" BETWEEN ('1986-01-01') and ('1986-12-31');
+--this ^ line is what I need to use for the other tables ,rather than dropping columns. 
